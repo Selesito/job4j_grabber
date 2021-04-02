6c76889bbaf4a85c85ba14aacae9e0e980c7002a
Selesito <selesitomorozov@gmail.com>
2021-04-02 17:42:27 +0400

    2.4. SqlRuParse [#462106]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,13 +4,14 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
     private static long idCounter = 0;
 
     public static void main(String[] args) throws Exception {
@@ -29,13 +30,26 @@ public class SqlRuParse {
         }
     }
 
-    public static Post parserPost(String url) throws IOException {
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> posts = new ArrayList<>();
+        Document doc = Jsoup.connect(link).get();
+        Elements row = doc.select(".postslisttopic");
+        for (int i = 0; i < row.size(); i++) {
+            Element href = row.get(i).child(0);
+            posts.add(detail((href.attr("href"))));
+        }
+        return posts;
+    }
+
+    @Override
+    public Post detail(String link) throws IOException {
         SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
-        Document doc = Jsoup.connect(url).get();
+        Document doc = Jsoup.connect(link).get();
         String title = doc.select(".messageHeader").get(0).text();
         String text = doc.select(".msgBody").get(1).text();
-        Element date = doc.select(".msgFooter").get(0);
-        String created = date.text().substring(0, 16);
-        return new Post(idCounter++, title, text, url, parser.parse(created));
+        String date = doc.select(".msgFooter").get(0).text();
+        String created = date.substring(0, date.indexOf('[') - 1);
+        return new Post(idCounter++, title, text, link, parser.parse(created));
     }
 }
